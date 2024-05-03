960a5d8bd274e91bc8f9bf6e5d76ae1cb1d88ed0
Ioannis Plakas <iplakas@nubificus.co.uk>
2024-05-03 15:32:09 +0300

    feat: Add Spt Testing
    * Added tests for ctr,crictl, and nerdctl.
    * For the ctr testing a spt-"no-net" image is used
    * For the nerdctl testing a spt-"redis" image is used,
    as well as for the crictl.
    
    Signed-off-by: Ioannis Plakas <iplakas@nubificus.co.uk>
    Reviewed-by: Charalampos Mainas <cmainas@nubificus.co.uk>
    Approved-by: Charalampos Mainas <cmainas@nubificus.co.uk>
    PR: https://github.com/nubificus/urunc/pull/26

--- tests/ctr/ctr_test.go
+++ tests/ctr/ctr_test.go
@@ -32,6 +32,29 @@ func TestCtrHvtRumprun(t *testing.T) {
 	}
 }
 
+func TestCtrSptRumprun(t *testing.T) {
+	containerImage := "harbor.nbfc.io/nubificus/urunc/hello-spt-nonet:latest"
+	containerName := "spt-rumprun-hello"
+	procName := "solo5-spt"
+	pullParams := strings.Fields("ctr image pull " + containerImage)
+	pullCmd := exec.Command(pullParams[0], pullParams[1:]...) //nolint:gosec
+	err := pullCmd.Run()
+	if err != nil {
+		t.Fatalf("Error pulling %s: %v", containerImage, err)
+	}
+	cmdString := fmt.Sprintf("ctr run --rm --snapshotter devmapper --runtime io.containerd.urunc.v2 %s %s", containerImage, containerName)
+	params := strings.Fields(cmdString)
+	cmd := exec.Command(params[0], params[1:]...) //nolint:gosec
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("%v: Error executing unikernel %s with %s using ctr: %s", err, containerName, procName, output)
+	}
+	expectedContain := "Hello world"
+	if !strings.Contains(string(output), expectedContain) {
+		t.Fatalf("Expected: %s, Got: %s", expectedContain, output)
+	}
+}
+
 func TestCtrQemuUnikraftNginx(t *testing.T) {
 	containerImage := "harbor.nbfc.io/nubificus/urunc/nginx-qemu-unikraft:latest"
 	containerName := "qemu-unikraft-nginx"
