c438382f27c9a355f9fd55b55f13956f504f223f
Charalampos Mainas <cmainas@nubificus.co.uk>
2025-01-15 16:08:58 +0000

    initial lupine
    Signed-off-by: Charalampos Mainas <cmainas@nubificus.co.uk>

--- pkg/unikontainers/unikernels/unikernel.go
+++ pkg/unikontainers/unikernels/unikernel.go
@@ -44,6 +44,9 @@ func New(unikernelType string) (Unikernel, error) {
 	case UnikraftUnikernel:
 		unikernel := newUnikraft()
 		return unikernel, nil
+	case LinuxUnikernel:
+		unikernel := newLinux()
+		return unikernel, nil
 	default:
 		return nil, ErrNotSupportedUnikernel
 	}
