0ccaca1d456d36747928ff7b1a0a50d8933788d5
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2025-02-06 15:33:53 +0000

    wip
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

--- packaging/urunc-deploy/buildah.sh
+++ packaging/urunc-deploy/buildah.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export PLATFORMS="linux/amd64"
+export PLATFORMS="linux/amd64,linux/arm64"
 export IMAGE="harbor.nbfc.io/nubificus/urunc/urunc-deploy"
-export TAG="0.4.0-rc5"
-buildah build --build-arg BRANCH=compat_kata_qemu --platform=$PLATFORMS --manifest "$IMAGE:$TAG" .
-# buildah build --build-arg BRANCH=compat_kata_qemu --jobs=2 --platform=$PLATFORMS --manifest "$IMAGE:$TAG" .
-buildah manifest push --all "$IMAGE:$TAG" "docker://$IMAGE:$TAG"
+export TAG="0.4.0-rc6"
+docker buildx build --build-arg BRANCH=compat_kata_qemu --platform $PLATFORMS --push -t $IMAGE:$TAG .
