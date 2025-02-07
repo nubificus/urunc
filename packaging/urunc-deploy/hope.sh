e6a7f2917140712dc0e75db6eb153ec6d124447c
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2025-02-07 17:03:08 +0200

    wip

--- packaging/urunc-deploy/hope.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PLATFORMS="linux/amd64,linux/arm64"
-export PLATFORMS="linux/arm64"
-export IMAGE="harbor.nbfc.io/nubificus/urunc/urunc-deploy"
-export TAG="0.4.0-rc6-buildah"
-# docker buildx build --build-arg BRANCH=compat_kata_qemu --platform $PLATFORMS --push -t $IMAGE:$TAG .
-sudo buildah build --jobs=1 --build-arg BRANCH=compat_kata_qemu --platform=$PLATFORMS --manifest "$IMAGE:$TAG" .
-# buildah build --build-arg BRANCH=compat_kata_qemu --jobs=2 --platform=$PLATFORMS --manifest "$IMAGE:$TAG" .
-sudo buildah manifest push --all "$IMAGE:$TAG" "docker://$IMAGE:$TAG"
